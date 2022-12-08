ad7ec995b649569e445ae736cb62c55078334348
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-08 20:04:00 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,1 +1,20 @@
-print("hello")
+# print("My name is Ayushi Jain")
+# print("I am a Python Trainer")
+# print("Today is out first demo session")
+
+# print(""" my name is ayushi
+# I am a python trainer""")
+
+# print("my name is ayushi \nI am a python trainer")
+
+# name = "1.2"
+# print(name)
+
+# print(type(name))
+
+
+
+number = 20+30
+print(number)
+
+# print(type(number))
