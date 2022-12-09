7da37e7c3441b220474f24a627fbf00abf8bb5ec
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-09 20:01:55 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,41 @@
+#This is a comment
+"""in the next line I am printing a statement
+I will use print funtion
+I will write hello"""
+#print("hello")
+
+#DataTypes-
+
+# String
+# Integer
+# Float/ Floating Points
+
+# ctrl+/ shortcut for comment
+# a = 1.2
+# print(type(a))
+
+# name = input("Enter your name here: ")
+# age = float(input("Enter your age here: "))
+# email = input("enter your email here: ")
+#
+# print(name)
+# print(age)
+# print(email)
+#
+# print(type(name))
+# print(type(age))
+# print(type(email))
+
+
+# num = eval(input("enter any math expression: "))
+# print(num)
+# print("3+6+7+9")
+
+# num1 = float(input("enter a number here: "))
+# num2 = float(input("enter another number here: "))
+#
+# print(num1+num2)
+
+x = 2
+y = 3
+print(x*3)
