94e8693a9237cc9aa8f7a23ec947aeb0bec4464b
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-10 20:03:36 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,49 @@
+#print(3+4+6-2 = 23)
+#x + y = 0
+#Operators = + - =
+#Operands = 3 4 6 2 23
+#print("hello")
+"""
+hello
+hi
+bye
+"""
+# print("""
+# hello
+# hi
+# bye""")
+
+# name = input("enter name")
+# print("welcome to our website",name, 1234)
+
+"""Task-
+take inputs from the user for student id
+and then print them systematically
+"""
+#Arithmatic Operators
+# print(3+4)
+# print(4-3)
+# print(3*4)
+# print(12/3)
+# print(4**3) #Exponentiation/power of a number
+# print(12%7) #Modulus
+# print(32//5) #Floor Division
+
+#Boolean Datatype = True/False
+#Comparison Operators
+
+# print(3>4) # Greater than
+# print(4<3) # Smaller than
+# print(4>=3) #Greater than and equal to
+# print(3<=4) #Smaller than and equal to
+# print (3==3) # equal to
+# print(3!=4) # not equal
+
+
+#Logical Operators
+# and gives True when both the conditions are true
+print(20 < 3 and 40 > 30 )
+# or gives true even if one condition is true
+print(20 < 3 or 40 > 30 )
+# not gives the exact opposite of any condition
+print(not(6 != 6))
