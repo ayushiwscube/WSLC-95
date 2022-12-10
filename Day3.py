3fa94f45ebeddc923579d25b362802e727f07556
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-10 20:38:28 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -47,3 +47,36 @@ print(20 < 3 and 40 > 30 )
 print(20 < 3 or 40 > 30 )
 # not gives the exact opposite of any condition
 print(not(6 != 6))
+
+
+
+
+
+
+#Recap from day 1
+# print("it's going to rain today")
+# print("wscubetech")
+# print("welcome in the first session")
+# print("""press 1 to add
+# press 2 to subtract
+# press 3 to multiply
+# press 4 to divide""")
+# print("hello \nhi \nbye")
+# 
+# score = 0
+# print("touch obstacle")
+# score = score - 2
+# print("killed enemy")
+# score = score + 5
+# 
+# print(score)
+
+
+#string = abcde... 12345... !@#$^
+#Anything inside "" is a string
+
+# a = 9571.555342
+# print(type(a))
+# name = input("Enter Username: ")
+# password = float(input("enter password: "))
+# print(password)
