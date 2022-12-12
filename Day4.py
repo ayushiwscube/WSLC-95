91817092254eba613d09b65ca60c48594dff75b0
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-12 20:12:23 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,37 @@
+#Assignment Operators
+# total = 100
+#
+# print("kiwi")
+# #Method1
+# total += 80
+# #Method2
+# total = total + 80
+#
+# print(total)
+#True and False are Boolean datatypes
+
+#Membership Operator
+
+# a = "Hello"
+#
+# print("l" in a)
+# print("l" not in a)
+
+#Identity Operator
+
+# a = "c"
+# b = "c"
+#
+# print(a is b)
+# print(a is not b)
+
+#Bitwise Operators
+print(bin(10))
+print(bin(15))
+print(bin(25))
+print(bin(8))
+
+print(10 & 8)
+print(10 | 8)
+print(10 ^ 8)
+print(bin(14))
