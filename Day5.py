827755b1b3be86b42b2974a5dc996b471780e924
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-13 20:02:37 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,83 @@
+"""
+ask user to choose between 1-4
+1 = addition
+2 = subtraction
+3 = multiplication
+4 = division
+ask num1
+ask num2
+
+if userchoice is equal to 1
+
+add them and display it to the user
+
+"""
+
+
+
+# marks = 90
+#Short hand if statement
+# if marks > 80:print("new phone")
+
+
+# #short hand if-else statement
+# marks = 90
+# if marks > 80:
+#     print("new phone")
+# else:
+#     print("new pulsar")
+#
+# print("even") if marks % 2 == 0 else print("odd")
+
+
+
+
+
+
+
+
+
+
+
+
+# username = "ayushi"
+# password = "12345"
+#
+# u = input("enter username: ")
+# if u == username and p == password :
+#     print("login successful")
+# else:
+#     print("incorrect username or password")
+#     p = input("enter password: ")
+#     if p == password:
+#         print("login successful")
+#     else:
+#         print("incorrect password")
+# else:
+#     print("incorrect username")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# marks = int(input("enter marks: "))
+# #indentation
+# if marks >90:
+#     print("trip")
+# elif (marks > 80 & marks < 90):
+#     print("new phone")
+# elif (marks > 70 & marks < 80):
+#     print("new laptop")
+# else:
+#     print("new pulsar")
