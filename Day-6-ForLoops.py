9e6a06fd4345177ab5e9562f9ed671d01a8e2fec
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-14 20:03:33 +0530

    Create Day-6-ForLoops.py

---
+++ Day-6-ForLoops.py
@@ -0,0 +1,91 @@
+# print("""press 1 for addition
+# press 2 for subtraction
+# press 3 for multiplication
+# press 4 for division""")
+#
+# choice = int(input("enter your choice from 1-4: "))
+#
+# if choice == 1:
+#     num1 = float(input("enter first number: "))
+#     num2 = float(input("enter second number: "))
+#     print(num1+num2)
+# elif choice ==2:
+#     num1 = float(input("enter first number: "))
+#     num2 = float(input("enter second number: "))
+#     print(num1-num2)
+# elif choice == 3:
+#     num1 = float(input("enter first number: "))
+#     num2 = float(input("enter second number: "))
+#     print(num1*num2)
+# elif choice == 4:
+#     num1 = float(input("enter first number: "))
+#     num2 = float(input("enter second number: "))
+#     print(num1/num2)
+# else:
+#     print("invalid input")
+
+
+
+#check if a number is positive of negative
+#check the number of digits in any input taken by the user upto 5
+
+# num = int(input("enter a number here: "))
+#
+# if num >0:
+#     print("positive")
+# else:
+#     print("negative")
+
+# num = int(input("enter a number here: "))
+#
+# if num >= -9 and num <=9:
+#     print("it is a single digit number")
+# elif (num>=10 and num<=99) or (num <= -10 and num >= -99 ):
+#     print("it is a 2 digit number")
+# elif num>=100 and num <= 999:
+#     print("it is a 3 digit number")
+# else:
+#     print("you have entered a number greater than 3 digits")
+
+
+
+
+
+
+
+
+
+
+# for i in range(0,21,2):
+#     print("song number",i, "is playing")
+
+
+# a = "wscubetech"
+# for i in a:
+#     print(i)
+
+# num = 2
+#
+# for i in range(1, 11):
+#     print(num,"x",i,"=",num*i)
+#
+# for i in range(1,11):
+#     print(num**i)
+
+#find the sum of all the even numbers from 1-50
+
+# sum = 0
+# for i in range(1,51,2):
+#     sum +=i
+#
+# print(sum)
+
+# a = "wscubetech"
+# count = 0
+# for i in a:
+#     count+=1
+#     
+# print(count)
+
+#print all the numbers divisible by 12 and 8 from (1-100)
+#(hint - use conditionl statement along with for loop)
