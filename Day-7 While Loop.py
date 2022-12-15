3142ca89529ee4ddf815f612099012c9075b4a61
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-15 19:55:42 +0530

    Create Day-7 While Loop.py

---
+++ Day-7 While Loop.py
@@ -0,0 +1,91 @@
+# for i in range(1,101):
+#     if i % 8 ==0 and i % 12 == 0:
+#         print(i)
+# n = 0
+# while n<=10:
+#     print(n)
+#     n= n+2
+
+# for i in range(0,11,2):
+#     print(i)
+# n = 1
+# while n <=10:
+#     print(n*17)
+#     n+=1
+
+# n = 1
+# while n <=10:
+#     print(n*17)
+#     n=n+1
+
+
+#find the sum of all the even sumbers upto 1-50
+
+# sum = 0
+# for i in range(0,51,2):
+#     sum = sum+i
+# print(sum)
+
+# sum=0
+# n = 0
+# while n<51:
+#     sum = sum+n
+#     n=n+2
+#
+# print(sum)
+
+
+# a = 0
+# while a<=15:
+#     print(a)
+#     a = a+1
+# a = 0
+# while True: #Infinite LOOP
+#     print(a)
+#     a = a+1
+
+#to stop a infinite loop one should use break statement
+#
+# while True:
+#     num1 = int(input("enter a number here: "))
+#     num2 = int(input("enter a number here: "))
+#     print(num1+num2)
+#     repeat = input("do you want to add more numbers?(yes/break) ")
+#     if repeat == "no" or repeat =="No" or repeat == "NO":
+#         break
+#
+# for i in range(1,21):
+#     print(i)
+#     break
+
+# name = input("enter customer's name: ")
+# total = 0
+# print("enter the price and quantity of the amount")
+# while True:
+#     price = float(input("item's price: "))
+#     quantity = float(input("item's quantity: "))
+#     total += price*quantity
+#     repeat = input("do you want to add more items(yes/no): ")
+#     if repeat == "no":
+#         break
+# 
+# print("-"*40)
+# print("customer's name: ", name)
+# print("total amount to be paid: ",total)
+# print("-"*40)
+
+"""
+ask name
+recliner = 400
+seat = 250
+
+ask number of seats 
+400*5
+2000
+or
+250*5
+1250
+
+(optional)-do you want to add a combo of popcorn rs = 200
+
+"""
