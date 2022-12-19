829357674697b6688e5bdb1adb07a6c66e724383
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-19 20:08:45 +0530

    Create Day-10 nestedLoops and string.py

---
+++ Day-10 nestedLoops and string.py
@@ -0,0 +1,75 @@
+# while True:
+#     name = input("enter customer's name: ")
+#     total = 0
+#     print("enter the price and quantity of the amount")
+#     while True:
+#         price = float(input("item's price: "))
+#         quantity = float(input("item's quantity: "))
+#         total += price*quantity
+#         repeat = input("do you want to add more items(yes/no): ")
+#         if repeat == "no":
+#             break
+#
+#     print("-"*40)
+#     print("customer's name: ", name)
+#     print("total amount to be paid: ",total)
+#     print("-"*40)
+#
+#     next_person = input("go to next person?(yes/no): ")
+#     if next_person=="no":
+#         break
+
+
+# for i in range(1,6): #rows
+#     for j in range(0,i):
+#         print("*", end = "")
+#     print()
+
+#Strings
+#indexing
+# a = "wscube tech"
+# print(type(a))
+# print(a[-1])
+# print(a[-5])
+
+
+#slicing
+
+# a = "wscubetech"
+#
+# print(a[2:6])
+# print(a[6:10])
+# print(a[-8:])
+# print(a[:6])
+#
+# b = "hello"
+# print(b[0:2])
+#
+# c = "123456789"
+# print(c[0:6])
+# print(c[::2])
+# print(c[::-1])
+
+
+
+# a = "hello"
+# print(a[::-1])
+# print(a[3::1])
+# print(a[3:0:-1])
+
+# num = input("enter number here: ")
+# 
+# print(num[::-1])
+
+#Task
+"""
+a = "Harry Potter"
+
+print only Letter P with 
+print only letter P with negative indexing
+print harry from the string
+print ter from the string
+print reverse of the string
+print harry in reverse
+
+"""
