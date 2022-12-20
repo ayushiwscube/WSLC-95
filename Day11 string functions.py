591d83833e524dabde90a1cfc60eff836f837c36
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-20 20:17:20 +0530

    Create Day11 string functions.py

---
+++ Day11 string functions.py
@@ -0,0 +1,76 @@
+
+
+# count = 0
+# for i in a:
+#     if i.isdigit() == True:
+#         count+=1
+# print(count)
+
+
+
+
+# print(a.count("e"))
+#
+# count = 0
+# for i in a:
+#     if i == "e":
+#         count+=1
+# print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+#print(len(a))
+
+# count = 0
+# for i in a:
+#     count += 1
+# print(count)
+
+
+
+a = "Harry123456hello124"
+# b = "#OOTD#LOL#TB#BTB"
+# c = "123456789"
+# d = "ayushi123"
+# e = "Harry Potter"
+# f = "HELLO1234*(^&^%^"
+# g = "hello"
+# h = "      "
+# i = "calculator"
+# print(g.center(11,"#"))
+# print(i.center(20,"`"))
+# print(e)
+#print(e.strip(" Potter")) #useful
+# print(e.split("r")) #useful
+# print(g.replace("hello","hi")) #useful
+# print(a.istitle())
+# print(e.istitle()) #useful
+# print(a.isspace())
+# print(f.isupper()) #useful
+# print(g.islower()) #useful
+# print(d.isalnum()) #useful
+# print(a.isalpha())
+# print(e.isalpha()) #useful
+# print(d.isdigit()) #useful
+# print(c.isdigit()) #useful
+# print(len(a)) #useful
+# print(a.count(" ")) #useful
+# print(b.count("#"))
+# print(a.upper()) #useful
+# print(a.lower()) #useful
+# print(a.swapcase())
+# print(a.capitalize()) #first letter will be capitalize only #useful
+# print(a.title()) #useful
+# print(a.casefold())
+# print(a.index("a"))
+# print(a[31])
+# print(a.count("h"))
