6b260251ffa68ab8caafc10e6d194903e4310a5e
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-12-21 20:09:33 +0530

    Create day-12 f-strings and lists.py

---
+++ day-12 f-strings and lists.py
@@ -0,0 +1,35 @@
+fruit = ["Kiwi","cherry","strawberry","orange","apple"]
+print(fruit)
+
+for i,j in enumerate(fruit):
+    print(i,j)
+
+
+# fruit2 = []
+# for i in fruit:
+#     fruit2.append(i)
+# print(fruit2)
+#
+# #list comprehension
+# fruit3 = [i for i in fruit]
+# print(fruit3)
+
+
+
+# fruit.append("banana")
+# print(fruit)
+# vegetable = []
+# vegetable.append("tomato")
+# print(vegetable)
+
+
+
+
+# for i in fruit:
+#     print(i)
+#enumerate method
+# for i,j in enumerate(fruit):
+#     print(i,j)
+
+# print (fruit)
+#list comprehension method
