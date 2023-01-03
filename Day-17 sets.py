715ecd5c07698ea11909826d6ae0a29810db58bc
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2023-01-03 20:02:34 +0530

    Create Day-17 sets.py

---
+++ Day-17 sets.py
@@ -0,0 +1,68 @@
+# a = [1,2,3,4,5]       
+# b = [2,4,6,8,10]      
+# c = [1,3,6,9,10,2]    
+#                       
+# print(list(set(a) & se
+#                       
+# z = [1,2,3]           
+#                       
+# z.append(4)           
+# print(z)              
+#                       
+                        
+                        
+# a = {5,9,1,2,3}       
+# b = {5,7,9}           
+#                       
+# print(a - b)          
+# & symbol is used as in
+# | symbol is used as un
+# ^ symbol is used as sy
+# - symbol is used as di
+                        
+                        
+                        
+#Typecasting = conversio
+# a = "12345"           
+# a = int(a)            
+# print(type(a))        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+# a = {5,9}             
+# b = {5,7,9}           
+                        
+#print(a.isdisjoint(b)) 
+# print(a.issubset(b))  
+# print(b.issuperset(a))
+                        
+                        
+# print(a.intersection(b
+# print(a.union(b))     
+# print(a.difference(b))
+# print(a.symmetric_diff
+                        
+# a.difference_update(b)
+# print(a)              
+                        
+                        
+                        
+                        
+                        
+                        
+# a = {3,5,2,4,6,3,2,4,5
+# print(a)              
+#                       
+# fruits = {"kiwi","appl
+# print(fruits)         
+# fruits.add("pineapple"
+# print(fruits)         
+#                       
+# fruits.pop()          
+# print(fruits)         
+#                       
+# fruits.discard("kiwi")
