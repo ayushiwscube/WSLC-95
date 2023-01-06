f7c55af2d512022eac0f4d129bc46e8bdfe906e8
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2023-01-06 20:19:04 +0530

    Create Day 19 Modules part 2.py

---
+++ Day 19 Modules part 2.py
@@ -0,0 +1,60 @@
+# import random
+# characters = "1234567890"
+#
+# length = int(input("enter length of the password: "))
+#
+# password = ""
+#
+# for i in range(0,length):
+#     char = random.choice(characters)
+#     print(char)
+#     password = password+char
+#     print(password)
+#
+# print(password)
+
+# import time
+#
+# print("hello world")
+# time.sleep(5)
+# print("my name is ayushi")
+
+# import datetime
+#
+# a = datetime.datetime.now()
+# print(a.year)
+# print(a.strftime("%G"))
+#
+# b = datetime.datetime(2023, 1, 14)
+# print(b.strftime("%A"))
+
+
+
+# import math
+
+# print(math.floor(2.3))
+# print(math.ceil(2.3))
+
+# print(max(13,56,78))
+# print(min(13,45,2))
+#
+# print(math.sin(30))
+
+#import random
+#
+# print(random.randint(1,100))
+
+import wscube as bunty
+bunty.add(34,78)
+bunty.mul(45,67)
+
+print(bunty.company["Name"])
+
+# a = 2
+# print(math.sqrt(a))
+# print(27**(1/3))
+#
+# print(math.pow(a,8))
+
+import twinkletwinkle
+twinkletwinkle.hello()
