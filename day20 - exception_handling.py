8dc0f923db06c71d393a15d3b9df3ce55b5d0568
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2023-01-09 20:05:01 +0530

    Create day20 - exception_handling.py

---
+++ day20 - exception_handling.py
@@ -0,0 +1,74 @@
+"""
+task- make calc program with 0 errors
+"""
+
+
+
+
+# try:
+#     import random
+# except :
+#     print("module not found")
+# finally:
+#     print("me toh dono me aaunga")
+
+# try:
+#     import ayushi
+# except :
+#     print("module not found")
+# else:
+#     print("koi error nhi aayi h")
+
+
+
+# try:
+#     l =["kiwi","apple","banana"]
+#     print(l[3])
+# except IndexError as a:
+#     print(a)
+
+
+# try:
+#     company = {"Name":"John","Age":23,"Gender":"Male"}
+#     print(company["DOB"])
+# except:
+#     print("the key in not present in dict")
+
+
+
+
+
+
+
+
+
+
+
+# try:
+#     num = int(input("enter number: "))
+#     num2 = int(input("enter number: "))
+#     print(num/num2)
+#
+# except (ZeroDivisionError,ValueError) as a:
+#     print(a)
+
+
+# print("hello world")
+#
+# name = input("enter username: ")
+
+# num = int(input("enter number: "))
+# num2 = int(input("enter number: "))
+# print(num/num2)
+
+# try:
+#     num = int(input("enter number: "))
+#     num2 = int(input("enter number: "))
+#     print(num/num2)
+#
+# except:
+#     print("the number should not be divided by zero")
+#
+# print("hello world")
+#
+# name = input("enter username: ")
