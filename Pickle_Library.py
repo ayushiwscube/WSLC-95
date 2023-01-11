083ddbe65d1adb1de2a2100bb96c77ee596865d3
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2023-01-11 20:06:38 +0530

    Create Pickle_Library.py

---
+++ Pickle_Library.py
@@ -0,0 +1,48 @@
+"""
+Library Management System
+file 1 = username password for the librarian
+ask librarian to display book, add a borrowed book, return a book, display borrowed books
+
+math = 9
+
+name =
+bookname = 
+
+"""
+
+
+
+# import pickle
+
+
+# username = input("enter username: ")
+# if username in my_dict.keys():
+#     password = input("enter password: ")
+#     if password == my_dict[username]:
+#         print("login successful")
+    
+
+
+#     else:
+#         print("incorrect password")
+# else:
+#     print("incorrect username")
+
+
+
+
+# import pickle
+#
+# #wb denotes write-binary
+# # company = {"Name":"john","Age":24,"Gender":"Male"}
+# # numbers = [2,4,5,6,7,89,9,0]
+# # f = open("demo.txt","wb")
+# # a = open("abc.txt","wb")
+# # pickle.dump(company,f)
+# # pickle.dump(numbers,a)
+# # f.close()
+# # a.close()
+#
+# f = open("demo.txt","rb")
+# xyz = pickle.load(f)
+# print(xyz)
